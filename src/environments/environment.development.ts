61e46026281dc5a14152e049c11b17a6f56c124d
Rougeux Max <rougeux.max@gmail.com>
2026-05-06 13:46:48 +0200

    init. commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  API_BASE_URL: "http://localhost:8080/api",
+  WS_BASE_URL: "localhost:8080/ws",
+  IMG_BASE_URL: "http://localhost:8080/uploads",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_BASE_URL: "http://192.168.1.11:8080/api",
+  WS_BASE_URL: "192.168.1.11:8080/ws",
+  IMG_BASE_URL: "http://192.168.1.11:8080/uploads",
+};
+
